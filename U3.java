f5b14d764a0aa4b389c83b5cbbe534cc1528233c
jananguiano <jananguiano0@gmail.com>
2026-03-25 08:32:20 -0600

    Prueba1

---
+++ U3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.scanner;
+public class U3;
+public static double
+calcularNeto (double bruto) {
+double tasaIva = 0.16;
+return bruto + (bruto*tasaIva);
+public static void main (String[] args){
+    Scanner sc = new;
+    scanner (System.in);
+    Int opcion;
+ System.out.println("=====Sistema de Auditoría para Tienda de Hardware======");   
+ System.out.println("1. Auditar Facturas del Día");
+ System.out.println("2. Calculadora de Impuestos (IVA)");
+ System.out.println("3. Cerrar Turno");
+ System.out.println("Seleccione una operación:");
+ opcion = sc.nextInt ();
+if (opcion == 1) {
+    System.out.println("¿Cuantas facturas se emitieron Hoy");
+    int cantidad = sc.nextInt();
+    double acumulado = 0;
+    for (int i = 1; <= cantidad; i++ )  {
+        System.out.println
+
+    }
+    
+}
+
+}    
+}
+
